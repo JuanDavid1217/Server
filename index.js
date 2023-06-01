0781e56a032439dd2275b9edae7d8bc3ee6fc4d4
juan <juand_david2001@hotmail.com>
2023-06-01 13:43:59 -0600

    my second commit

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const cors = require('cors')
 const express = require('express')
 
-const { MongoClient} = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://JuanDavid1217:JuanDavid#1712@cluster0.m3ei4fv.mongodb.net/?retryWrites=true&w=majority";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri);
-/*async function run() {
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
@@ -18,7 +24,6 @@ const client = new MongoClient(uri);
   }
 }
 run().catch(console.dir);
-*/
 
 const app = express();
 app.use(cors());
