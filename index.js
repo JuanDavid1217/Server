a7be31f0fb62bda5cfe1ad85de946cd4cd558cf9
juan <juand_david2001@hotmail.com>
2023-06-01 17:34:52 -0600

    i

--- index.js
+++ index.js
@@ -4,21 +4,26 @@ const express = require('express')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://JuanDavid1217:JuanDavid%231712@cluster0.m3ei4fv.mongodb.net/?retryWrites=true&w=majority";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri);
-/*async function run() {
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    await client.db("fisicoculturismo").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
-run().catch(console.dir);*/
-
+run().catch(console.dir);
 const app = express();
 app.use(cors());
 app.options('*', cors());
@@ -34,11 +39,9 @@ app.get('/reactionsbypublication', (req, res, next)=>{
   try{
     console.log(publication)
     console.log(reaction)
-    client.connect();
-    client.db("fisicoculturismo").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    res.send('Hola Todo esta bien')
   }catch(error){
-    res.send({'error':'hay un error'})
+    res.send('Hola soy un error')
   }finally{
     conexion.close();
   }
