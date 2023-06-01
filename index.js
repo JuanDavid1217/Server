a20bd19f5586f9365014b9871920e9c049a11dfd
juan <juand_david2001@hotmail.com>
2023-06-01 13:07:06 -0600

    my second commit

--- index.js
+++ index.js
@@ -4,13 +4,7 @@ const express = require('express')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://JuanDavid1217:JuanDavid#1712@cluster0.m3ei4fv.mongodb.net/?retryWrites=true&w=majority";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+const client = new MongoClient(uri);
 /*async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
