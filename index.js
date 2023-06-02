0086f41a91c8acc945f502f3c01440ac908e7236
juan <juand_david2001@hotmail.com>
2023-06-02 00:46:22 -0600

    se corrigen errores

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const express = require('express')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://JuanDavid1217:JuanDavid%231712@cluster0.m3ei4fv.mongodb.net/?retryWrites=true&w=majority";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri)/*, {
+const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
   }
-});*/
+});
 /*async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -38,23 +38,31 @@ app.get('/', (req, res, next) => {
   res.send('Mongo api - CreazyDave');
 });
 
-app.get('/reactionsbypublication', (req, res)=>{
+app.get('/reactionsbypublication', async (req, res)=>{
   const publication=req.query.publication
-  const reaction=req.query.reaction
   try{
-    const data={"_id.publication":publication}
     client.connect()
-    const db = client.db("fisicoculturismo")
-    const result = db.collection("reactions_summary").find({"_id.publication":`${publication}`})//.find(data)
-    console.log(JSON.stringify(result))
-    //console.log(publication)
-    //console.log(reaction)
-    //console.log(client.db("fisicoculturismo").collection("reactions_summary").find({"_id.publication":publication}))
+    const data={"_id.publication":publication}
+    const db = await client.db("fisicoculturismo").collection("reactions_summary").find(data).toArray()
+    res.send(db)
   }catch(error){
-    res.send('Hola soy un error'+error)
+    res.send({'status':2})
+  }
+
+})
+
+app.get('/commentsbypublication', async (req, res)=>{
+  const publication=req.query.publication
+  try{
+    client.connect()
+    const data={"publication":publication}
+    const db = await client.db("fisicoculturismo").collection("comments_info").find(data).toArray()
+    res.send(db)
+  }catch(error){
+    res.send({'status':2})
   }
 })
+
 app.listen(port,  () => 
 	console.log('listening on port ' + port
 ));
-
